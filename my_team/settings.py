2907bfaa618fdff5486e83404f87910c76bce6c5
sabpasit <148407315+Sabpasit666@users.noreply.github.com>
2024-09-11 17:04:52 +0700

    ลบ

--- my_team/settings.py
+++ my_team/settings.py
@@ -132,5 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 
 LOGIN_REDIRECT_URL = 'home'  # URL ที่คุณต้องการให้ผู้ใช้ไปหลังจาก login สำเร็จ
-LOGOUT_REDIRECT_URL = 'login'  # URL ที่คุณต้องการให้ผู้ใช้ไปหลังจาก logout สำเร็จ
 LOGOUT_REDIRECT_URL = 'map'  # URL ที่คุณต้องการให้ผู้ใช้ไปหลังจาก logout สำเร็จ
