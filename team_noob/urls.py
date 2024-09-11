f34c46ccc5233aba427a71b4d14f4653bee93616
sabpasit <148407315+Sabpasit666@users.noreply.github.com>
2024-09-11 16:31:38 +0700

    ลบล็อกอิน

--- team_noob/urls.py
+++ team_noob/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.indexPage, name= 'index'),
     path('continue/', views.continue_view, name='continue'),
-    path("login/", views.loginPage, name= 'login'),
     path("map/", views.mappage, name= 'map'),
 
 ]
